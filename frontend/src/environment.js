1c7ca04febb64faa58758215ffbc3b7927d55252
Anshika Srivastava <anshika82d@gmail.com>
2025-08-14 00:38:57 +0530

    Deployment for frontend

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,6 +1,11 @@
 let IS_PROD = true;
-const server = IS_PROD ?
-    "https://zoom-clone-backend-970g.onrender.com/" :
+const server = IS_PROD 
+
+    ?
+
+    "https://zoom-clone-backend-970g.onrender.com/" 
+    
+    :
 
     "http://localhost:8000"
 
